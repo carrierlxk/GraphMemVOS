4c10eea8d3ac70357ccaf650f92053e8e3a4418e
Lu Xiankai <carrierlxk@gmail.com>
2020-08-31 10:33:37 +0400

    Update run_graph_memory_test.sh

--- run_graph_memory_test.sh
+++ run_graph_memory_test.sh
@@ -1,3 +1,3 @@
 #python eval_DAVIS_graph_memory.py -g '4' -s val -y 17 -D /raid/DAVIS/DAVIS-2017/DAVIS-train-val
 #python eval_DAVIS.py -g '0' -s val -y 16 -D /media/xiankai/Data/segmentation/DAVIS-2016
-python runfiles/eval_DAVIS_graph_memory.py -c './workspace_STM_alpha/main_runfile_graph_memory_alpha_ep0079.pth.tar'
+python runfiles/eval_DAVIS_graph_memory.py -c './workspace_STM_alpha/main_runfile_graph_memory.pth.tar'
