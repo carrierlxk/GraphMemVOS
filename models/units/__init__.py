bbfada81526ee4f8f2489f902a38e284e5d597e3
Lu Xiankai <carrierlxk@gmail.com>
2020-08-31 10:06:05 +0400

    Add files via upload

---
+++ models/units/__init__.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/media/xiankai/Data/segmentation/OSVOS/ECCV_graph_mem/models/units')
+#from LinkFunction import LinkFunction
+from MessageFunction import MessageFunction
+from UpdateFunction import UpdateFunction
+#from ReadoutFunction import ReadoutFunction
+#'LinkFunction', , 'ReadoutFunction'
+__all__ = ('MessageFunction', 'UpdateFunction')
