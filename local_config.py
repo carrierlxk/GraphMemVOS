af9decb820e5805a0a67f379e8e546018022e1da
Lu Xiankai <carrierlxk@gmail.com>
2020-08-31 10:00:12 +0400

    Add files via upload

---
+++ local_config.py
@@ -0,0 +1,15 @@
+
+
+# Put these paths to point to your datasets
+config = {
+    'workspace_path' : "/home/ubuntu/xiankai/STM_train_V1/",
+    'davis_path' : "/home/ubuntu/xiankai/dataset/DAVIS-2016/",
+    'davis16_path' : "/fastdata/davis/2016/",
+    'davis17_path' : "/raid/DAVIS/DAVIS-2017/DAVIS-train-val/",
+    'ytvos_path' : "/raid/Youtube-VOS-2019/",
+    'output_path' : "/raid/STM_train_V1/output/",
+    'nn_weights_path' : "/home/cgv841/gwb/Code/agame-vos-master/"
+}
+
+
+
